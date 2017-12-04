c5ed9459829db6eb8f285ef48be6ccf29f59c343
bemzj <qiuzijia520@qq.com>
2017-12-04 17:22:24 +0800

    1241722

--- js/apply.js
+++ js/apply.js
@@ -1,4 +1,19 @@
-$(function(){    
+$(function(){ 
+	//省市区三级联动
+	!function () {
+        var $target = $('#city');
+
+        $target.citySelect();
+
+        $target.on('click', function (event) {
+            event.stopPropagation();
+            $target.citySelect('open');
+        });
+
+        $target.on('done.ydui.cityselect', function (ret) {
+            $(this).val(ret.provance + ' ' + ret.city + ' ' + ret.area);
+        });
+    }();
     //失去焦点
     $('#city').on('click',function(){
     	$(this).blur();
